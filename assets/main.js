0e61b172f69586ece9296c370472252b934f8b16
PCHIII <treyhelmer@gmail.com>
2019-08-01 12:37:56 -0400

    updated portfolio page

---
+++ assets/main.js
@@ -0,0 +1,19 @@
+// $(document).ready(function() {
+    
+//    console.log("document is ready");
+     
+   
+//      $( ".card" ).hover(
+//      function() {
+//        $(this).addClass('shadow-lg'); 
+//      }, 
+     
+//      function() {
+//        $(this).removeClass('shadow-lg');
+//      }
+//    );
+     
+   
+//    });
+   
+   
